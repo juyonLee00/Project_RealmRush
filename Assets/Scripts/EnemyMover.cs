1309c427f3a94e6cafdaaf5ddccb9b9b33c6f75b
juyonLee00 <ljy012900@gmail.com>
2023-02-18 21:08:45 +0900

    #1 적이 경로 찾고 따라가도록 설정 & endpoint 도달 시 사라짐

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -11,7 +11,26 @@ public class EnemyMover : MonoBehaviour
     
     void Start()
     {
+        FindPath();
+        ReturnToStart();
         StartCoroutine(FollowPath());
+    }
+
+    void FindPath()
+    {
+        path.Clear();
+
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+        foreach(GameObject waypoint in waypoints)
+        {
+            path.Add(waypoint.GetComponent<Waypoint>());
+        }
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
     }
 
     IEnumerator FollowPath()
@@ -31,5 +50,8 @@ public class EnemyMover : MonoBehaviour
                 yield return new WaitForEndOfFrame();
             }
         }
+
+        Destroy(gameObject);
+
     }
 }
